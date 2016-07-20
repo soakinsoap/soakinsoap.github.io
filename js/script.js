9f9d0240c6d90bf41c2a6f888a32eb008d942438
soakinsoap <morganchrisp@gmail.com>
2016-07-20 03:28:07 -0400

    add expand button function

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var expand = "+";
+  var collapse = "–";
+
+  $(".btn-expand").click(function(){
+
+    $(".collapsable").toggle();
+
+    var button = $(".btn-expand");
+
+    if(button.text() == collapse){
+      console.log("collapse");
+      button.text(expand);
+    }
+    else{
+      console.log("expand");
+      button.text(collapse);
+    }
+  });
+});
